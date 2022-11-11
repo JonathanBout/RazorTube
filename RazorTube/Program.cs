3db044e07ff8d9d191d2aff86a961e000c3d61ba
Jonathan Bout <jonathanbout01@gmail.com>
2022-11-11 18:54:12 +0100

    Updated URL

--- RazorTube/Program.cs
+++ RazorTube/Program.cs
@@ -5,7 +5,7 @@ using RazorTube.Services;
 using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseUrls("http://127.0.0.1:5000");
+builder.WebHost.UseUrls("http://127.0.0.1:5000/");
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
