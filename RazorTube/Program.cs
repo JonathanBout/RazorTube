f05cb93289ffc856049007e09f2e23c0ac0838d2
Jonathan Bout <jonathanbout01@gmail.com>
2022-11-11 15:53:27 +0100

    removed https

--- RazorTube/Program.cs
+++ RazorTube/Program.cs
@@ -7,20 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped<VideoService>();
-builder.WebHost.UseUrls("https://127.0.0.1:5000");
+builder.WebHost.UseUrls("http://127.0.0.1:5000");
 var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
-{
-    app.UseHsts();
-}
 
 app.UseForwardedHeaders(new ForwardedHeadersOptions
 {
     ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
 });
-
-app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
